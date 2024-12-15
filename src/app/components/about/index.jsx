bf51edd8e1e90c3f2c5bda61113b04809d376693
MEHEDI HASAN PARASH <112463517+mastermhp@users.noreply.github.com>
2024-12-15 07:22:49 +0600

    about update

--- src/app/components/about/index.jsx
+++ src/app/components/about/index.jsx
@@ -76,7 +76,7 @@ const AboutDetails = () => {
         <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
           <img
             className="w-full h-auto"
-            src={`${process.env.NEXT_PUBLIC_GITHUB_STREAK_STATS_URL}?user=mastermhp&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B`}
+            src={'https://streak-stats.demolab.com/?user=mastermhp&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B'}
             alt="mastermhp"
             loading="lazy"
           />
